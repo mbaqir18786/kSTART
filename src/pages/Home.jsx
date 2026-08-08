14c36e977bff3cf39f588ffec6f86537db80cd81
Mohammed Baqir Bandarkar <baqirab224@gmail.com>
2026-08-08 12:45:02 +0530

    style(ui): curve arrow in opposite arc towards button and make arrow completely static

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -479,14 +479,14 @@ export default function Home() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              d="M80 8 C 70 35, 40 45, 15 58"
+              d="M80 10 C 85 48, 45 65, 15 58"
               stroke="currentColor"
               strokeWidth="2.5"
               strokeLinecap="round"
               fill="none"
             />
             <path
-              d="M26 50 L 15 58 L 24 66"
+              d="M26 48 L 15 58 L 26 64"
               stroke="currentColor"
               strokeWidth="2.5"
               strokeLinecap="round"
